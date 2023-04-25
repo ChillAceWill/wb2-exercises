5ba86cf89c0b3e343a57d810dce32a3e43f0f687
Gerald Williams <gwilliams@appdev.yearup.org>
2023-04-25 11:50:07 -0400

    Updated rule_of_72

--- mathscripts/rule_of_72.js
+++ mathscripts/rule_of_72.js
@@ -1,2 +1,11 @@
 // Interest on savings account
-var startingValue =;
+var rule = 72;
+var returnRate = 4;
+var money = 2500;
+var double = money * 2;
+
+// Calculations
+var time = rule / returnRate;
+
+// Output
+console.log;
