a11b774248a45ec46933d88c058703e18126a23a
Gerald Williams <gwilliams@appdev.yearup.org>
2023-04-25 10:09:43 -0400

    Initial Commit

---
+++ hello-world/hello-world.js
@@ -0,0 +1,21 @@
+//Exercise 1
+//letters, numbers,$,_
+//assign "customerId" to console.log
+var customerId;
+var firstName = 'Gerald ';
+var middleName = 'D ';
+var lastName = 'Williams ';
+var gender = 'he/him ';
+var dob = '07/01/94 ';
+var dlicense = '0000000 ';
+var autopolicy = '00000 '
+var customerId= firstName + '' + middleName + '' + lastName + ''
+ + gender + '' + dob + '' + dlicense + '' + autopolicy;
+
+
+
+
+
+
+// print message
+console.log(customerId);
