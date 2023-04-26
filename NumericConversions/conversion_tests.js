1f7e8f1d115ddce32e56bab88652d3957d5216fc
Gerald Williams <gwilliams@appdev.yearup.org>
2023-04-26 15:20:57 -0400

    Updated files

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,11 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+var b = +b;
+
+console.log(b);
